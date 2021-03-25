83304034ca5a522039a2bb5aa0ca6ff8b81deb47
yb00 <yagobraz98@gmail.com>
2021-03-24 22:00:19 -0300

    Adiciona no_parallel.py

---
+++ no_parallel.py
@@ -0,0 +1,61 @@
+import time
+import csv
+
+from collections import Counter
+from functools import reduce
+
+def read(path):
+    with open(path, newline='') as f:
+        reader = csv.reader(f, delimiter=';')
+        data = list(reader)
+    # for row in data:
+    #     print(row[30])
+    return data[1:]
+
+# mapper
+def count(rows):
+    key_map = Counter()
+    for row in rows:
+        key = row[18]
+        if key in key_map:
+            key_map[key] += 1
+        else:
+            key_map[key] = 1
+    return key_map
+
+# reducer
+def merge(A, B):
+    return A + B
+
+def split(data, parts):
+    return (data[i::parts] for i in range(parts))
+
+def run():
+    num_splits = 4
+    
+    # Leitura
+    path = './mg.csv'
+    data = read(path)
+    
+    # Divisão
+    data_chunks = list(split(data, num_splits))
+    
+    # Processamento
+    mapper = count
+    reducer = merge
+    
+    temp = Counter()
+    for chunk in data_chunks:
+        key_map = count(chunk)
+        temp = merge(temp, key_map)
+        
+    reduced = temp
+    for key in reduced:
+        print(f'{key}: {reduced[key]}')
+    
+if __name__ == '__main__':
+    start = time.time()
+    run()
+    end = time.time()
+    print(f'Total runtime: {end - start}')
+    
